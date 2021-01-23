72c5b633fbda0923ba33d0f33ccf20621689d656
Nitish Kumar <krnitish@live.in>
2021-01-23 15:48:40 +0530

    Selector Added

--- src/Palatte.js
+++ src/Palatte.js
@@ -5,21 +5,25 @@ import NavBar from './NavBar'
 class Palatte extends Component {
     constructor(props) {
         super(props)
-        this.state = { level: 500 }
+        this.state = { level: 500, format: "hex" }
         this.changeLevel = this.changeLevel.bind(this)
+        this.changeColor = this.changeColor.bind(this)
     }
     changeLevel(level) {
         this.setState({ level })
     }
+    changeColor(val) {
+        this.setState({ format: val })
+    }
     render() {
         const { colors } = this.props.palette
-        const { level } = this.state
+        const { level, format } = this.state
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} />
         ))
         return (
             <div className='Palette'>
-                <NavBar level={level} changeLevel={this.changeLevel} />
+                <NavBar level={level} changeLevel={this.changeLevel} handleChange={this.changeColor} />
                 {/* TODO: Nav bar goes here */}
                 <div className='Palette-color'>
                     {colorBoxes}
